c25a7fbb97e95f71188565916255492839738d23
vanppo <vanppo@hotmail.com>
2020-12-01 15:25:38 +0800

    Add core plugins settings

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const theme = require('./theme')
 const { getCustomProperties, transformConfig } = require('./utils')
 
+const corePlugins = {
+  backgroundOpacity: false,
+  borderOpacity: false,
+  divideOpacity: false,
+  placeholderOpacity: false,
+  ringOpacity: false,
+  textOpacity: false,
+}
+
 const config = {
+  corePlugins,
   theme,
 }
 
