6c14cdac02c40adebf6e4a4507e7ff0da59af916
moonRider <solar.pom@gmail.com>
2020-12-02 22:49:57 +0800

    feat: use disabled variant to colors

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ const corePlugins = {
   textOpacity: false,
 }
 
+const variantsExtend = {
+  backgroundColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
+  borderColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
+  boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
+  cursor: ['responsive', 'disabled'],
+  divideColor: ['responsive', 'dark', 'disabled'],
+  placeholderColor: ['responsive', 'dark', 'focus', 'disabled'],
+  textColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled']
+}
+
 const config = {
   corePlugins,
   theme,
+  variants: {
+    extend: variantsExtend,
+  },
 }
 
 module.exports = {
