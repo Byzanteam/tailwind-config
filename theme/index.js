cc9bd76bd4ca841a35255d2cbd844bc50e9a2210
moonRider <solar.pom@gmail.com>
2020-12-02 22:40:57 +0800

    feat: add spacing and radius config

--- theme/index.js
+++ theme/index.js
@@ -3,6 +3,8 @@ const colors = require('./colors')
 const fontFamily = require('./font-family')
 const fontSize = require('./font-size')
 const fontWeight = require('./font-weight')
+const borderRadius = require('./radius')
+const spacing = require('./spacing')
 
 module.exports = {
   boxShadow,
@@ -10,4 +12,6 @@ module.exports = {
   fontFamily,
   fontSize,
   fontWeight,
+  borderRadius,
+  spacing,
 }
