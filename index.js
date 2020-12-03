d102e0674744ebca9db44b883ce981913014df76
moonRider <solar.pom@gmail.com>
2020-12-03 14:07:10 +0800

    feat: update variants extend config

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ const corePlugins = {
 }
 
 const variantsExtend = {
-  backgroundColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
-  borderColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
-  boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled'],
+  backgroundColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled', 'even', 'odd', 'active'],
+  borderColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled', 'active'],
+  boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled', 'active'],
   cursor: ['responsive', 'disabled'],
   divideColor: ['responsive', 'dark', 'disabled'],
   placeholderColor: ['responsive', 'dark', 'focus', 'disabled'],
-  textColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled']
+  textColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus', 'disabled', 'active'],
+  opacity: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'active', 'disabled'],
+  ringColor: ['responsive', 'dark', 'focus-within', 'focus', 'active', 'hover', 'disabled'],
+  textDecoration: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'active', 'visited'],
+  transformOrigin: ['hover', 'responsive'],
+  zIndex: ['responsive', 'focus-within', 'focus', 'hover', 'active'],
 }
 
 const config = {
