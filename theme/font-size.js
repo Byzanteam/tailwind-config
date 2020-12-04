248be5841d9b8f5e40a3bb6b43f896ae6c75af21
vanppo <vanppo@hotmail.com>
2020-12-04 16:41:59 +0800

    Update font configurations in theme

--- theme/font-size.js
+++ theme/font-size.js
@@ -1,13 +1,11 @@
+// TODO: set default lineHeight for different font sizes
+
 module.exports = {
-  h1: ['24px', { lineHeight: '36px' }],
-  h2: ['22px', { lineHeight: '34px' }],
-  h3: ['20px', { lineHeight: '30px' }],
-  h4: ['18px', { lineHeight: '28px' }],
-  h5: ['16px', { lineHeight: '24px' }],
-  h6: ['14px', { lineHeight: '20px' }],
-  title: ['18px', { lineHeight: '24px' }],
-  subtitle: ['16px', { lineHeight: '24px' }],
-  body1: ['14px', { lineHeight: '20px' }],
-  body2: ['13px', { lineHeight: '20px' }],
-  caption: ['12px', { lineHeight: '18px' }],
+  xs: ['0.75rem', { lineHeight: '1.5' }],
+  sm: ['0.875rem', { lineHeight: '1.5' }],
+  base: ['1rem', { lineHeight: '1.5' }],
+  lg: ['1.125rem', { lineHeight: '1.5' }],
+  xl: ['1.25rem', { lineHeight: '1.5' }],
+  '2xl': ['1.5rem', { lineHeight: '1.5' }],
+  '3xl': ['1.875rem', { lineHeight: '1.5' }],
 }
--- theme/index.js
+++ theme/index.js
@@ -2,10 +2,12 @@ const boxShadow = require('./box-shadow')
 const colors = require('./colors')
 const fontFamily = require('./font-family')
 const fontSize = require('./font-size')
+const lineHeight = require('./line-height')
 
 module.exports = {
   boxShadow,
   colors,
   fontFamily,
   fontSize,
+  lineHeight,
 }
