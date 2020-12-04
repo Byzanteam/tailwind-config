ded030cfde5a61783d0dc75d8ff8b4291d175700
vanppo <vanppo@hotmail.com>
2020-12-04 17:02:51 +0800

    Set default line height for different font sizes

--- theme/font-size.js
+++ theme/font-size.js
@@ -1,11 +1,9 @@
-// TODO: set default lineHeight for different font sizes
-
 module.exports = {
-  xs: ['0.75rem', { lineHeight: '1.5' }],
-  sm: ['0.875rem', { lineHeight: '1.5' }],
-  base: ['1rem', { lineHeight: '1.5' }],
-  lg: ['1.125rem', { lineHeight: '1.5' }],
-  xl: ['1.25rem', { lineHeight: '1.5' }],
-  '2xl': ['1.5rem', { lineHeight: '1.5' }],
-  '3xl': ['1.875rem', { lineHeight: '1.5' }],
+  xs: ['0.75rem', { lineHeight: '1rem' }],
+  sm: ['0.875rem', { lineHeight: '1.25rem' }],
+  base: ['1rem', { lineHeight: '1.5rem' }],
+  lg: ['1.125rem', { lineHeight: '1.75rem' }],
+  xl: ['1.25rem', { lineHeight: '2rem' }],
+  '2xl': ['1.5rem', { lineHeight: '2.25rem' }],
+  '3xl': ['1.875rem', { lineHeight: '2.5rem' }],
 }
